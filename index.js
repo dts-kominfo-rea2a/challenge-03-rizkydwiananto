d4f71a9ec31075f5e5c22f2cd9e82938772d3f9d
rizkydwiananto <rizkydwiananto@gmail.com>
2022-05-31 21:01:59 +0700

    New Challenge Rizky Dwi Ananto Function

---
+++ index.js
@@ -0,0 +1,60 @@
+// ! JANGAN DIMOFIDIKASI
+const dataBelanjaan = [
+  {
+    id: "D-10001",
+    nama: "Minyak Goreng Delima",
+    harga: 25000,
+    kuantitas: 2,
+  },
+  {
+    id: "D-10020",
+    nama: "Beras Mamos",
+    harga: 48000,
+    kuantitas: 1,
+  },
+  {
+    id: "F-00010",
+    nama: "Larutan Cap Kaki Empat",
+    harga: 7500,
+    kuantitas: 8,
+  },
+];
+
+// boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
+const listBelanjaan = () => {
+  let arrBelanja = [];
+  for (let index = 0; index < dataBelanjaan.length; index++) {
+    arrBelanja.push(
+      `- ${dataBelanjaan[index].nama} x ${dataBelanjaan[index].kuantitas}`
+    );
+  }
+  return arrBelanja;
+};
+
+// boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
+const totalBelanjaan = (dataBelanjaan) => {
+  let totalBelanja = 0;
+  for (let index = 0; index < dataBelanjaan.length; index++) {
+    totalBelanja += dataBelanjaan[index].harga * dataBelanjaan[index].kuantitas;
+  }
+  return totalBelanja;
+};
+
+// ! JANGAN DIMODIFIKASI
+const main = () => {
+  console.log("Selamat datang customer");
+
+  console.log("Belanjaan Anda adalah:");
+  console.log(listBelanjaan(dataBelanjaan));
+  console.log(
+    "\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan(dataBelanjaan)
+  );
+};
+
+main();
+
+module.exports = {
+  dataBelanjaan,
+  listBelanjaan,
+  totalBelanjaan,
+};
